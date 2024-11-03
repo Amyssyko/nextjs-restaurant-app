3a0584f6e8391c4b9f6d567249e2ef0cc12bdd39
Amyssyko <cristian-0005@live.com>
2024-11-02 21:23:23 -0500

    Agregar encabezados de seguridad y configuración de CORS en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,40 @@
-import { NextConfig } from 'next'
+import type { NextConfig } from 'next'
+const cspHeader = `
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+const nextConfig: NextConfig = {
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value: cspHeader.replace(/\n/g, '')
+					},
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: '*' // Set your origin
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET'
+					}
+				]
+			}
+		]
+	},
 
-const nextConfig: NextConfig = {}
+	experimental: {
+		dynamicIO: false
+	}
+}
 
 export default nextConfig
